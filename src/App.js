11065dfc6f4c4095ab1e6a0c31af98ce66613207
smaurya64 <smaurya64@gmail.com>
2022-06-26 19:26:23 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <>
+        <p>
+
+Our new website 
+          Vivek memorial trust
+        </p>
+        
+          New today
+       
+    </>
+  );
+}
+
+export default App;
