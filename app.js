2fe583e2a8614076e37a88c3b2412d7d56092989
smaurya64 <41261144+smaurya64@users.noreply.github.com>
2022-06-26 20:26:01 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+        <p>
+
+Our new website 
+          Vivek memorial trust
+        </p>
+        
+          New today
+       
+    </>
+  );
+}
+
+export default App;
